27e1a870d57ae0fad33552d3e0b07a85770cd5fe
Ricardo Gomes <sleiph@gmail.com>
2021-05-14 08:46:14 -0300

    mudando o form principal

--- MRP_Fatec/Painel.cs
+++ MRP_Fatec/Painel.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using MRP_Fatec.Elementos;
@@ -51,36 +44,63 @@ namespace MRP_Fatec
         }
 
         /// funções dos botões
-        private void btn_cadastro_pecas_Click(object sender, EventArgs e)
+        private void CadComp_BtnClick(object sender, EventArgs e)
         {
             cadastro_componentes.Main_form cadastro_form = new cadastro_componentes.Main_form();
             cadastro_form.ShowDialog();
         }
 
-        private void BtnControledeEstoqpProd_Click(object sender, EventArgs e)
+        private void EstComp_BtnClick(object sender, EventArgs e)
         {
             Controle_de_estoque_prod.Estoque_prod frm = new Controle_de_estoque_prod.Estoque_prod();
             frm.ShowDialog();
         }
 
-        private void btn_compras_Click(object sender, EventArgs e)
+        private void Compras_BtnClick(object sender, EventArgs e)
         {
             ModuloCompras.Form1 frm = new ModuloCompras.Form1();
             frm.ShowDialog();
         }
 
-        private void BtnCadProdutoFinal_Click(object sender, EventArgs e)
+        private void CadProd_BtnClick(object sender, EventArgs e)
         {
             MRPPROJETO.FormCadProdFinal frm = new MRPPROJETO.FormCadProdFinal();
             frm.ShowDialog();
         }
 
-        private void btn_estoqueFinal_Click(object sender, EventArgs e)
+        private void EstProd_BtnClick(object sender, EventArgs e)
         {
             // gambiarra pra substituir o modulo fantasma
             ///string caminho = Application.StartupPath;
             ///string exe = caminho + @"\..\..\..\modulos\5estoque_produtos\mrp_template.exe";
             ///Process.Start(exe);
+            mrp_template.main_form frm = new mrp_template.main_form();
+            frm.ShowDialog();
+        }
+
+        /// funções do menu
+        private void CadComp_MenuClick(object sender, EventArgs e)
+        {
+            cadastro_componentes.Main_form cadastro_form = new cadastro_componentes.Main_form();
+            cadastro_form.ShowDialog();
+        }
+        private void EstComp_MenuClick(object sender, EventArgs e)
+        {
+            Controle_de_estoque_prod.Estoque_prod frm = new Controle_de_estoque_prod.Estoque_prod();
+            frm.ShowDialog();
+        }
+        private void Compras_MenuClick(object sender, EventArgs e)
+        {
+            ModuloCompras.Form1 frm = new ModuloCompras.Form1();
+            frm.ShowDialog();
+        }
+        private void CadProd_MenuClick(object sender, EventArgs e)
+        {
+            MRPPROJETO.FormCadProdFinal frm = new MRPPROJETO.FormCadProdFinal();
+            frm.ShowDialog();
+        }
+        private void EstProd_MenuClick(object sender, EventArgs e)
+        {
             mrp_template.main_form frm = new mrp_template.main_form();
             frm.ShowDialog();
         }
@@ -91,5 +111,6 @@ namespace MRP_Fatec
             // carrega form de login
             ChamaLogin(); // desativar pra facilitar os testes
         }
+
     }
 }
